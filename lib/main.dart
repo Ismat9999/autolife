dfd8e13331344bfefa40fe86d2197c84c43544ad
rakhmatovsanjar <sanjarbekrahmatov2003@gmail.com>
2025-08-23 01:49:50 +0500

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:autolife/presantation/auth/sign_up/pages/enter_email_screen.dart';
+
+import 'package:autolife/presantation/blocs/home/home_bloc.dart';
+import 'package:autolife/presantation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
       },
       child: BlocProvider(
         create: (context)=> HomeBloc(),
-        child: HomePage(),
+        child: const HomePage(),
       ),
     );
   }
